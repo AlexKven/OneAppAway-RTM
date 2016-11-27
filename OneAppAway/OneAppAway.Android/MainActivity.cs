86f12802a9cd0d326fed612251495a7dca6daef0
Alexander Kvenvolden <mathandprogramming@gmail.com>
2016-11-26 22:21:07 -0800

    11-26-2016 File-New day for Android version!

---
+++ OneAppAway/OneAppAway.Android/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using Android.Content.PM;
+
+namespace OneAppAway.Android
+{
+    [Activity(Label = "OneAppAway.Android", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.Holo.Light.NoActionBar")]
+    public class MainActivity : Activity
+    {
+        private Button button1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            //// Remove title bar
+            //RequestWindowFeature(WindowFeatures.NoTitle);
+
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            button1 = (Button)this.FindViewById(Resource.Id.button1);
+        }
+
+        //public override bool OnCreateOptionsMenu(IMenu menu)
+        //{
+        //    MenuInflater inflator = MenuInflater;
+        //    inflator.Inflate(Resource.Menu.MainMenu, menu);
+        //    return false;
+        //}
+    }
+}
+
